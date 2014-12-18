86028420b8dbe742e7d1d961efdd558335f369b8
Panphobia <Panphobia@users.noreply.github.com>
2014-12-17 23:14:12 -0500

    .

--- ProjectEuler156.java
+++ ProjectEuler156.java
@@ -1,15 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package projecteulersol;
 import java.math.*;
 public class ProjectEuler156 extends Library {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         BigInteger s = BigInteger.ZERO;
         long f[]=new long[10];
@@ -17,11 +7,8 @@ public class ProjectEuler156 extends Library {
             f(f,i);
             for(int j=1;j<10;++j)
             if(f[j]==i)s = s.add(BigInteger.valueOf(f[j]));
-            
-            //System.out.println(i+" "+f[1]);
         }
         System.out.println(s);
-        
     }
     public static void f(long[]f,int n){
         while(n>0){
