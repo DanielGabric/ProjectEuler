c06dfeb99efc085982698143c4ac875a2a92f058
Panphobia <Panphobia@users.noreply.github.com>
2014-12-17 23:54:08 -0500

    .

---
+++ ProjectEuler137.java
@@ -0,0 +1,15 @@
+import java.math.*;
+public class ProjectEuler137 {
+    public static void main(String[] args) {
+        BigInteger fibSum=BigInteger.ZERO;
+        BigInteger fib1=BigInteger.ONE;
+        BigInteger fib2=BigInteger.ONE;
+        for(int i=3;i<=31;++i){
+            fibSum = fib1.add(fib2);
+            fib2 = fib1;
+            fib1 = fibSum;
+        }
+        System.out.println(fib1.multiply(fib2));
+    }
+    
+}
