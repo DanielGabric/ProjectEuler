6546fa64fbb8a9a90acfb4dba8a2b8ab07961cba
Panphobia <Panphobia@users.noreply.github.com>
2014-12-17 23:38:43 -0500

    .

---
+++ ProjectEuler301.java
@@ -0,0 +1,12 @@
+//Solution is the 32nd fibonacci number
+public class ProjectEuler301 {
+    public static void main(String[] args) {
+        long fib=0,fib1=0,fib2=1;
+        for(int i=0;i<=30;++i){
+            fib=fib1+fib2;
+            fib1=fib2;
+            fib2=fib;
+        }
+        System.out.println(fib);
+    }
+}
