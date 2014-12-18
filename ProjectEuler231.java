0e2880fdb8c293bd9f1dfabd99eb437762f574db
Panphobia <Panphobia@users.noreply.github.com>
2014-12-17 23:14:57 -0500

    .

--- ProjectEuler231.java
+++ ProjectEuler231.java
@@ -1,16 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package projecteulersol;
-
 import java.util.*;
 public class ProjectEuler231 extends Library{
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         ArrayList<Integer>primes = sieveN(2,20000000);
         int n = 20000000;
