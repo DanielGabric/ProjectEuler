85d49ca119f17eacbe88624ceb21ebe5b391432c
Panphobia <Panphobia@users.noreply.github.com>
2014-12-17 23:45:07 -0500

    .

---
+++ ProjectEuler214.java
@@ -0,0 +1,14 @@
+public class ProjectEuler214 {
+    public static void main(String[] args) {
+        long N = 40000000;
+        long[]t = Library.listTotients(N);
+        int[]l=new int[(int)N+1];
+        long sum = 0;
+        l[0]=0;
+        out:for(int start=1;start<=N;++start){
+            l[start]=l[(int)t[start]]+1;
+            if(l[start]==25&&t[start]+1==start)sum+=start;
+        }
+        System.out.println(sum);
+    }
+}
