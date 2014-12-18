874ff9f275a1b0c8d84b0b531742d81a128b19e1
Panphobia <Panphobia@users.noreply.github.com>
2014-12-17 23:13:28 -0500

    .

--- ProjectEuler157.java
+++ ProjectEuler157.java
@@ -1,8 +1,5 @@
-package projecteulersol;
-
 import java.math.BigInteger;
 import java.util.*;
-
 public class ProjectEuler157 extends Library {
     public static void main(String[] args) {
         double first = System.currentTimeMillis();
