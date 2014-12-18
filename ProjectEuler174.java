6757d2264f2fb67bddfcc364c03d1c564d58af7b
Panphobia <Panphobia@users.noreply.github.com>
2014-12-17 23:50:08 -0500

    .

---
+++ ProjectEuler174.java
@@ -0,0 +1,16 @@
+public class ProjectEuler174 {
+    public static void main(String[] args) {
+        int[]tiles=new int[1000001];
+        for(int t =1;t<=250000;++t){
+            int sum = 0;
+            for(int row = t+2;;row+=2){
+                sum+=4*row-4;
+                if(sum>1000000)break;
+                tiles[sum]++;
+            }
+        }
+        int sum=0;
+        for(int i=1;i<tiles.length;++i)if(tiles[i]>=1&&tiles[i]<=10)sum++;
+        System.out.println(sum);
+    }
+}
