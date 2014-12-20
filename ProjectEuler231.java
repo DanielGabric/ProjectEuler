a62314e5e97c9191827ea425d61ce95e6c4979bf
Panphobia <Panphobia@users.noreply.github.com>
2014-12-20 01:23:44 -0500

    .

--- ProjectEuler231.java
+++ ProjectEuler231.java
@@ -1,5 +1,11 @@
 import java.util.*;
 public class ProjectEuler231 extends Library{
+    //If you think back to getting the amount of 0's at the end of a factorial
+    //This is very similar
+    //In effect when you were getting 0's at the end of factorial, you were successifvely dividing
+    //N by powers of 5, and this got you the final numbers of factors of 5 in the N!
+    //By using this property we can get the exponents of any prime in N! by
+    //dividing by powers of the Prime P
     public static void main(String[] args) {
         ArrayList<Integer>primes = sieveN(2,20000000);
         int n = 20000000;
