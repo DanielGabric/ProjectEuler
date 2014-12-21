08d6f14a1925336d3a0f6adea7ee467d73c74510
Panphobia <Panphobia@users.noreply.github.com>
2014-12-21 16:07:04 -0500

    .

---
+++ ProjectEuler207.java
@@ -0,0 +1,9 @@
+public class ProjectEuler207 {
+    public static void main(String[] args) {
+        double i = 1, r = 1, q = 1.0 / 12345.0;
+        do {
+            r = (int) (Math.log(++i + 1) / Math.log(2)) / (i - 1);
+        } while (r >= q);
+        System.out.println((i) * (i - 1));
+    }
+}
