4142baaaaee20a6179b138c9afe6a868239cb15f
Panphobia <Panphobia@users.noreply.github.com>
2014-12-25 09:05:22 -0500

    .

--- ProjectEuler172.java
+++ ProjectEuler172.java
@@ -1,7 +1,10 @@
 public class ProjectEuler172 extends Library {
     static long n = 0;
+    //Save 17! to avoid calculating it every time
     public static final long SEVENTEEN = f(17);
     public static void main(String[] args) {
+        //Loop through every amount of digits 0-9 to
+        //a maximum of 3 digits
         for (int a = 0; a < 4; ++a) {
             for (int b = 0; b < 4; ++b) {
                 for (int c = 0; c < 4; ++c) {
@@ -28,12 +31,24 @@ public class ProjectEuler172 extends Library {
     }
 
     public static void z(int[] a, int sum) {
+        //if the amount of digits is not 18 then just go back and try again
+        //until it is
         if (sum != 18)return;
         long m;
+        //If there are no 0's in this number then there are 18 choices for the first digit
         if (a[0] == 0)m = 18;
+        //If there are 0's in the number then the number of choices for the first digit
+        //since 0 cannot be a first digit is 18 choice minus the numbers of 0's
         else m = 18 - a[0];
+        //multiply the number of digits allowed for first digit by 17!
         m = m * SEVENTEEN;
+        //Example
+        //If there is more than one digit per number than divide by
+        //The number of digits! since there is no order in the
+        //same digits
         for (int i = 0; i < 10; ++i)m /= f(a[i]);
+        //add the amount of solutions with this configuration of
+        //numbers to the total
         n += m;
     }
 }
