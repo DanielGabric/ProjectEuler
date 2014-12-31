9b5d19ecccf4e7aa165248551c831ae7b91bcd53
Panphobia <Panphobia@users.noreply.github.com>
2014-12-31 14:04:08 -0500

    .

--- ProjectEuler159.java
+++ ProjectEuler159.java
@@ -6,19 +6,26 @@ public class ProjectEuler159 extends Library {
         int[] mdrs = new int[1000000];
         long s = 0;
         for (int i = 2; i < 1000000; ++i) {
+            //Getting factors of i
             ArrayList<Integer> f = Library.getFactorsWithout1(i);
 
+            //Set the initial value at index i, to be the digital root sum of i
             mdrs[i] = getDS(i);
             for (int j = 0; j < f.size() / 2; ++j) {
+                //2 factors of i
                 int a = f.get(2*j);
                 int b = f.get(2*j+1);
+                //Either the sum of the minimum of the digital root sums of a and b are the min
+                //or the digital root sum of i is the min
                 mdrs[i] = Math.max(mdrs[i], mdrs[a] + mdrs[b]);
             }
+            //increment min
             s += mdrs[i];
         }
         System.out.println(s);
     }
 
+   //Digital root sum
     public static int getDS(int n) {
         int r = n%9;
         return r==0?9:r;
