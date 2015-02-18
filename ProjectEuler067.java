9149f6016ff7069d1b356c53eecb661ad2f5a21b
MiscProgrammingProblems <MiscProgrammingProblems@users.noreply.github.com>
2015-02-18 12:54:42 -0500

    .

---
+++ ProjectEuler067.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class ProjectEuler67 {
+    public static void main(String[] args) throws FileNotFoundException{
+        Scanner s = new Scanner(new File("p067_triangle.txt"));
+        int[][]triangle = new int[100][100];
+        for(int i=0;i<100;++i)
+            for(int j=0;j<=i;++j)
+                triangle[i][j]=s.nextInt();
+        for(int i=98;i>=0;--i)
+            for(int j=0;j<=i;++j)
+                triangle[i][j]+=Math.max(triangle[i+1][j], triangle[i+1][j+1]);
+        System.out.println(triangle[0][0]);
+    }
+}
