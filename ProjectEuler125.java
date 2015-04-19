8dc9d688f1dd048d215965821de0c89bc71f9fc7
DanielGabric <DanielGabric@users.noreply.github.com>
2015-04-18 22:31:54 -0400

    .

--- ProjectEuler125.java
+++ ProjectEuler125.java
@@ -1,7 +1,7 @@
 public class ProjectEuler125 {
     public static void main(String[] args) {
         int limit = (int)Math.pow(10, 8);
-        int[]pali=new int[limit+1];
+        boolean[]pali=new boolean[limit+1];
         int sqrt = (int)Math.sqrt(limit);
         long c=0;
         for(int i=1;i<=sqrt;++i){
@@ -9,10 +9,10 @@ public class ProjectEuler125 {
             for(int j=i+1;j<=sqrt;++j){
                 sum+=j*j;
                 if(sum>limit)break;
-                if(p(sum))pali[sum]++;
+                if(p(sum))pali[sum]=true;
             }
         }
-        for(int i=0;i<limit+1;i++)if(pali[i]>0)c+=i;
+        for(int i=0;i<limit+1;i++)if(pali[i])c+=i;
         System.out.println(c);
     }
     public static boolean p(int N){
