d59ff09ca115434765559d3ec1ba18a8fefccc1e
Dan <danielgabric@live.com>
2015-05-02 03:32:39 -0400

    .

---
+++ ProjectEuler291.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.math.*;
+public class ProjectEuler291{
+    public static void main(String[]args){
+        BigInteger limit = BigInteger.TEN;
+        limit = limit.pow(15);
+        limit = limit.multiply(BigInteger.valueOf(5));
+        long c=0;
+        for(long i=1;;++i){
+            BigInteger b = BigInteger.valueOf(i);
+            b = (b.multiply(b)).add((b.add(BigInteger.ONE)).multiply(b.add(BigInteger.ONE)));
+            if(b.compareTo(limit)==1)break;
+            if(b.isProbablePrime(10))c++;
+        }
+        System.out.println(c);
+    }
+}
