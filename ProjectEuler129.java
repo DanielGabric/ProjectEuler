03622d2e584f895de442c3916b1fdac35bf6eff7
Dan <danielgabric@live.com>
2015-05-03 01:40:08 -0400

    .

---
+++ ProjectEuler129.java
@@ -0,0 +1,13 @@
+//repunit of length k = (10^k-1)/9
+import java.util.*;
+import java.math.*;
+public class ProjectEuler129{
+    public static void main(String[]args){
+        for(long n=1000001;;n+=2){
+            if(n%5==0)n+=2;
+            long k=1;
+            for(long repunit=1;repunit !=0;repunit=(repunit*10+1)%n,k++);
+            if(k>1000000){System.out.println(n);break;}
+        }
+    }
+}
