be8c0ecff0ee6eed1301d14852466016a9c92762
Dan <danielgabric@live.com>
2015-05-05 23:36:45 -0400

    .

---
+++ ProjectEuler206.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+typedef long long int LONG;
+int main(){
+    LONG start = 1010101010,end = 1389026623,i,check[10] = {0,9,8,7,6,5,4,3,2,1};
+    for(i=start;i<=end;){
+        LONG result = i*i,j=0;
+        while(result>0){
+            if(result % 10 != check[j])
+                goto out;
+            result /=100;
+            j++;
+        }
+        printf("%lli\n",i);
+        break;
+        out:
+        i++;
+    }
+    return 0;
+}
