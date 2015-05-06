f1e4bd14bd5b8bae933ad4434e3a1d6bd07ada26
Dan <danielgabric@live.com>
2015-05-05 22:58:35 -0400

    .

--- ProjectEuler187.java
+++ ProjectEuler187.java
@@ -1,7 +1,7 @@
 //Sieve
 public class ProjectEuler187 {
     public static void main(String[] args) {
-        ArrayList<Integer> primes = sieveN(2,(int)Math.pow(10,8)/2+100000);
+        ArrayList<Integer> primes = sieveN(2,(int)Math.pow(10,8)/2+1);
         int[]factors = new int[(int)Math.pow(10,8)+5];
         for(int i=0;i<primes.size();++i)
             for(int j=0;primes.get(i)*primes.get(j)<=Math.pow(10, 8);++j)
