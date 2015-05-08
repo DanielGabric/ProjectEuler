0452f48e0428876027c90b93d2878867d1fc207e
Dan <danielgabric@live.com>
2015-05-08 02:14:04 -0400

    .

---
+++ ProjectEuler088.py
@@ -0,0 +1,19 @@
+def rec(sums,product,k):
+    if sums < k:return False
+    if k==1 and product == sums: return True
+    if product ==1 and k==sums: return True
+    j=2
+    while j <=product and sums-j>=k-1:
+        if product %j==0 and rec(sums-j,product/j,k-1)==True:return True
+        j+=1
+    return False
+tot=0
+saved = []
+for i in range(2,12001):
+    j=i+1
+    while rec(j,j,i)==False:
+        j+=1
+    if j not in saved:
+        tot+=j
+    saved.append(j)
+print tot
