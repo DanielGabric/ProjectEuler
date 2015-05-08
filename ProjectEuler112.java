b65b66e214f3bffe4e5077b8d0b01726c47984c1
Dan <danielgabric@live.com>
2015-05-07 23:55:37 -0400

    .

--- ProjectEuler112.java
+++ ProjectEuler112.java
@@ -1,24 +1,16 @@
 public class ProjectEuler112 {
     public static void main(String[] args) {
-        int nonBounce = 100;
         double bounce = 0;
         for(int i=101;true;++i){
             String n = Integer.toString(i);
-            int bigger=0;
-            int smaller =0;
-            int same =0;
+            int bigger=0,smaller =0;
             for(int x=0;x<n.length()-1;++x){
                 if(n.charAt(x)<n.charAt(x+1))
                     smaller++;
                 else if (n.charAt(x)>n.charAt(x+1))
-                    bigger++;
-                else 
-                    same++;
-            }
+                    bigger++;            }
             if(smaller > 0 && bigger > 0)
                 bounce ++; 
-            else 
-                nonBounce++;
             if(bounce/i == 0.99){
                 System.out.println(i);
                 break;
