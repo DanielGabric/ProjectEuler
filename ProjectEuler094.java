0752b7803a59fd461ee28b554bd4fb0db33e8a63
Dan <danielgabric@live.com>
2015-05-11 13:39:44 -0400

    .

--- ProjectEuler094.java
+++ ProjectEuler094.java
@@ -4,13 +4,10 @@ public class ProjectEuler94 {
         for (long i = 3; i <= Math.floor(1000000000 / 3); i+=2) {
             long b = (i - 1) / 2;
             long b1 = (i + 1) / 2;
-            if (s((i * i - b * b))) {
+            if (s((i * i - b * b))) 
                 sum += (long) (3 * i - 1);
-            }
-            if (s((i * i - b1 * b1))) {
+            if (s((i * i - b1 * b1))) 
                 sum += (long) (3 * i + 1);
-
-            }
         }
         System.out.println(sum);
     }
--- ProjectEuler178.py
+++ ProjectEuler178.py
@@ -2,8 +2,10 @@ prev = [-1]*1500000
 START = 10
 END = 40
 totSum=0
-
 def rec(num_digits, current_digit, allDigits):
+    #stores a binary string og length 10, starts out as 1111111111
+    #if a digit has been seen, it eliminates a 1, if the string is 0
+    #the rule has been met
     binRep = 1 << current_digit
     if num_digits == 1:
         if allDigits == 0 or allDigits == binRep:
@@ -14,6 +16,9 @@ def rec(num_digits, current_digit, allDigits):
     hash1 = current_digit+10*num_digits+1000*allDigits
     if prev[hash1]!=-1:
         return prev[hash1]
+    #checks if the digit has already been seen
+    #if it hasn't it takes away that 1 from the binary
+    #string
     if allDigits & binRep != 0:
         allDigits -= binRep
     if current_digit > 0:
@@ -22,7 +27,6 @@ def rec(num_digits, current_digit, allDigits):
         sum1 += rec(num_digits-1,current_digit+1,allDigits)
     prev[hash1]=sum1
     return sum1
-
 for i in range(START,END+1):
     for j in range(1,10):
         totSum += rec(i,j,1023)
