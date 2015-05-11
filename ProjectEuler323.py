07439fb9747b2c32aa2ca3c6b0742cc66a136c37
Dan <danielgabric@live.com>
2015-05-11 03:24:18 -0400

    .

--- ProjectEuler323.py
+++ ProjectEuler323.py
@@ -1,1 +1,1 @@
-print sum([(1.0 - (1.0 - (1.0/2.0)**i)**32.0) for i in range(200)])
+print sum([(1.0 - (1.0 - (1.0/2.0)**i)**32.0) for i in range(50)])
---
+++ ProjectEuler504.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+#define M 100
+typedef long long int L;
+L gcd(L x, L y) {
+    while (y != 0) {
+        L z = x % y;
+        x = y;
+        y = z;
+    }
+    return x;
+}
+L isSquare(L something){
+    L f = (L)sqrt(something);
+    if(f*f==something)return 1;
+    else return 0;
+}
+int main(){
+    L a,b,c,d,sum=0;
+    for(a=1;a<=M;++a)
+        for(b=1;b<=M;++b)
+            for(c=1;c<=M;++c)
+                for(d=1;d<=M;++d)
+                    if(isSquare((a*b+b*c+c*d+d*a-gcd(a,b)-gcd(b,c)-gcd(c,d)-gcd(d,a))/2+1))
+                        sum ++;
+    printf("%lli\n",sum);
+    return 0;
+}
