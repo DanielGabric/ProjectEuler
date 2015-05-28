47658a544529de760637784e6ac3cde959e1ac5b
Dan <danielgabric@live.com>
2015-05-28 01:34:39 -0400

    counting

--- ProjectEuler158.py
+++ ProjectEuler158.py
@@ -1,2 +1,2 @@
 def factorial(n):return reduce(lambda x,y:x*y,[1]+range(1,n+1))
-print max(((2**n-n-1)*factorial(26)//(factorial(n)*factorial(26-n)))for n in range(2,26))
+print max(((2**n-n-1)*factorial(26)//(factorial(n)*factorial(26-n)))for n in range(2,26)) 
