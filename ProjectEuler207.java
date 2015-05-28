df7dbd5e1f606f0ff26a8d413892c92ece7294ac
Dan <danielgabric@live.com>
2015-05-28 01:39:17 -0400

    ad hoc

--- ProjectEuler207.java
+++ ProjectEuler207.java
@@ -5,5 +5,5 @@ public class ProjectEuler207 {
             r = (int) (Math.log(++i + 1) / Math.log(2)) / (i - 1);
         } while (r >= q);
         System.out.println((i) * (i - 1));
-    }
+    } 
 }
