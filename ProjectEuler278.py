51b2cc18e6071e7124d64809c6ade64e921d34e4
Dan <danielgabric@live.com>
2015-05-28 01:42:48 -0400

    number theoretical observations

--- ProjectEuler278.py
+++ ProjectEuler278.py
@@ -5,4 +5,4 @@ for p in range(0,len(primes)):
     for q in range(p+1,len(primes)):
         for r in range(q+1,len(primes)):
             tot+=2*primes[p]*primes[q]*primes[r]-primes[p]*primes[q]-primes[q]*primes[r]-primes[r]*primes[p]
-print tot
+print tot 
