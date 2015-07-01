dbaef031242f9a22f66764f3ab7c0585b0a29e88
Dan <danielgabric@live.com>
2015-07-01 16:34:16 -0400

    fib q-matrix/modular arithmetic

--- ProjectEuler435.py
+++ ProjectEuler435.py
@@ -1,7 +1,9 @@
 import Library
 MOD = 1307674368000
 N = 10**15
-T = [[0,0,0],[0,0,1],[0,1,1]]
+T = [[0,0,0],
+     [0,0,1],
+     [0,1,1]]
 sums=0
 print sum([(Library.pow(T,N+1,(x*x+x-1)*MOD)[1][1]*Library.powMod(x,N+2,(x*x+x-1)*MOD)+
             Library.pow(T,N+2,(x*x+x-1)*MOD)[1][1]*Library.powMod(x,N+1,(x*x+x-1)*MOD)-x)/((x*x+x-1))%MOD 
