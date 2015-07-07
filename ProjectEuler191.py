ee916db98390da8a01bb0c00cf73766d12563807
Dan <danielgabric@live.com>
2015-07-07 13:32:24 -0400

    top-down memoization

--- ProjectEuler191.py
+++ ProjectEuler191.py
@@ -1,6 +1,6 @@
 cache={}
 possible = ["O","L","A"]
-def rec(N,lates,lastTwo,R=""):
+def rec(N,lates,lastTwo):
     sums=0
     if N==0:
         return 1
@@ -9,11 +9,11 @@ def rec(N,lates,lastTwo,R=""):
         return cache[key]
     for i in possible:
         if i=="L" and lates < 1:
-            sums+=rec(N-1,lates+1,lastTwo[1::]+i,R+i)
+            sums+=rec(N-1,lates+1,lastTwo[1::]+i)
         elif i=="A" and lastTwo != "AA":
-            sums+=rec(N-1,lates,lastTwo[1::]+i,R+i)
+            sums+=rec(N-1,lates,lastTwo[1::]+i)
         elif i=="O":
-            sums+=rec(N-1,lates,lastTwo[1::]+i,R+i)
+            sums+=rec(N-1,lates,lastTwo[1::]+i)
     cache[key]=sums
     return sums
 print rec(30,0,"FF")
