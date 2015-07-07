e2a31910966b2818db2647df539620386faa5721
Dan <danielgabric@live.com>
2015-07-06 21:59:35 -0400

    q-matrix

--- ProjectEuler237.py
+++ ProjectEuler237.py
@@ -1,4 +1,4 @@
-import Library
+from Library import mul, pow
 T = [[0,1,0,0],[0,0,1,0],[0,0,0,1],[1,-2,2,2]]
 F = [[1],[1],[4],[8]]
-print Library.mul(Library.pow(T,10**12-1,10**8),F,10**8)[0][0]
+print mul(pow(T,10**12-1,10**8),F,10**8)[0][0]
