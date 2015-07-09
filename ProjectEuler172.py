4bdd540c291c06d3e2c9c1772bed91a1e9950b8d
Daniel Gabric <dgabric@mail.uoguelph.ca>
2015-07-09 13:21:02 -0400

    both dp and counting solutions

--- ProjectEuler172.py
+++ ProjectEuler172.py
@@ -1,3 +1,24 @@
+###########DP solution############
+cache={}
+def rec(A=[0,0,0,0,0,0,0,0,0,0]):
+    sums=0
+    if sum(A)==18:
+        return 1
+    key = tuple(A)
+    if key in cache:
+        return cache[key]
+    for i in xrange(10):
+        if sum(A)==0 and i==0:continue
+        if A[i] == 3:continue
+        M = list(A)
+        M[i]+=1
+        sums+=rec(M)
+    cache[key]=sums
+    return sums
+print rec()
+##################################
+
+###counting###
 n=0
 def factorial(n):return reduce(lambda x,y:x*y,[1]+range(1,n+1))
 def z(a,s):
@@ -25,3 +46,4 @@ for a in range(4):
                                         array = [a,b,c,d,e,f,g,h,i,j] 
                                         n+=z(array,sum(array))
 print n
+##########
