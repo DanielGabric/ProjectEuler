7f50fb2af21f15e24fd8383250e99431c21d0d28
Dan <danielgabric@live.com>
2015-11-25 23:37:47 -0500

    bottom-up DP with combinatorial generation

--- ProjectEuler164.py
+++ ProjectEuler164.py
@@ -1,12 +1,12 @@
 DP = [[0 for i in xrange(100)]for i in xrange(21)]
 for i in xrange(100,1000):
-    if i%10+(i/10)%10+(i/100)<=9:
+    if i%10+(i/10)%10+(i/100)>=9:
         DP[3][i%100]+=1
 for i in xrange(4,21):
     for j in xrange(0,100):
         s=j%10+(j/10)
-        if s <= 9:
-            for k in xrange(0,10-s):
-                if s+k <= 9:
-                    DP[i][(j%10)*10+k]+=DP[i-1][j]
-print sum(DP[20])
+        
+        for k in xrange(0,10):
+            if s+k >= 9:
+                DP[i][(j%10)*10+k]+=DP[i-1][j]
+print sum(DP[7])
--- ProjectEuler189.c
+++ ProjectEuler189.c
@@ -1,65 +1,42 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define LIMIT 7
-typedef long long int LONG;
-int power(int a,int b){
-    if(b==1) return a;
-    int x = power(a,b>>1);
-    x = (x*x);
-    if((b&1==1)==1)x = (x*a);
-    return x;
-}
+#define LIMIT 8
+long long int ways[14348907][LIMIT+1]={0},sum=0;
+int upper[20],lower[20],j;
 int getLevel(int n){
-    return 2*n+1;
+    return 2*n-1;
 }
-int main(){
-    LONG **F=malloc(sizeof(LONG*)*(LIMIT+1)),sum=0;
-    int i,j,k,x;
-    int totLimit = power(3,15);
-    for(i=0;i<=LIMIT;++i){
-        F[i]=malloc(sizeof(LONG)*(totLimit+1));
-        for(j=0;j<=totLimit;++j)
-            F[i][j]=0;
-    }
-    F[0][0]=F[0][1]=F[0][2]=1;
-    int currentRow[15]={0};
-    int rowBefore[15]={0};
-    for(i=1;i<=LIMIT;++i){
-        int depth=getLevel(i);
-        int depth1 = getLevel(i-1);
-        int firstLimit = power(3,depth);
-        int secondLimit = power(3,depth1);
-        for(j=0;j<secondLimit;++j){
-            if(F[i-1][j]==0)continue;
-            int flag=0;
-            int N = j;
-            for(x=0;x<depth1;++x){
-                rowBefore[x]=N%3;
-                N/=3;
-            }
-            for(k=0;k<firstLimit;++k){
-                flag=0;
-                int R = k;
-                for(x=0;x<depth;++x){
-                    currentRow[x]=R%3;
-                    if(x>0){
-                        if((currentRow[x]==currentRow[x-1])||(x%2==1 &&rowBefore[x-1]==currentRow[x])){
-                            flag=1;
-                            break;
-                        }
-                    }
-                    R/=3;
-                }
-                if(flag)continue;
-                F[i][k]+=F[i-1][j];
-            }
+void generate(int last, int c,int n){
+    int i;
+    if(c>=getLevel(n)){
+        int upperBase3=0,lowerBase3=0,power3=1;
+        for(i=getLevel(n)-1,power3=1;i>=0;--i,power3*=3)lowerBase3+=power3*lower[i];
+        for(i=getLevel(n-1)-1,power3=1;i>=0;--i,power3*=3)upperBase3+=power3*upper[i];
+        ways[lowerBase3][n]+=ways[upperBase3][n-1];
+    }else{
+        for(i=0;i<3;++i){
+            if(last == i ||(c>0 && c%2==1 &&upper[c-1]==i))continue;
+            lower[c]=i;
+            generate(i,c+1,n);
         }
     }
-    for(i=0;i<=totLimit;++i)
-        sum+=F[LIMIT][i];
+}
+void recurse(int last, int c, int n){
+    int i;
+    if(c>=getLevel(n-1))
+        generate(-1,0,n);
+    else{
+        for(i=0;i<3;++i){
+            if(last==i)continue;
+            upper[c]=i;
+            recurse(i,c+1,n);
+        }
+    }
+
+}
+int main(){
+    ways[0][1]=ways[1][1]=ways[2][1]=1;
+    for(j=2;j<=LIMIT;++j)recurse(-1,0,j);
+    for(j=0;j<14348907;++j)sum+=ways[j][LIMIT];
     printf("%lld\n",sum);
-    for(i=0;i<=LIMIT;++i)
-        free(F[i]);
-    free(F);
     return 0;
 }
