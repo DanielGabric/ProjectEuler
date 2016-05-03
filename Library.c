1a66fa1cfa513960f04523b5ceaedf6e74ee3708
Dan <danielgabric@live.com>
2016-05-03 18:41:47 -0400

    bruteforce

--- Library.c
+++
@@ -1,8 +0,0 @@
-#include "Library.h"
-
-void sieve(int primes[],int N){
-    int i,j;
-    primes[0]=primes[1]=-1;
-    for(i=2;i<N;++i)primes[i]=1;
-    for(i=2;i<=N;++i)if(primes[i])for(j=i;j<=N;j+=i)primes[i]=0;
-}
