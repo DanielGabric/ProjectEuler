0ef0945d8e76d6cbd8c0dca3ccfd36892a7ec5ba
Dan <danielgabric@live.com>
2016-05-03 12:33:17 -0400

    C helper library

---
+++ Library.c
@@ -0,0 +1,8 @@
+#include "Library.h"
+
+void sieve(int primes[],int N){
+    int i;
+    primes[0]=primes[1]=-1;
+    for(i=2;i<N;++i)primes[i]=1;
+    for(i=2;i<=N;++i)if(primes[i])for(j=i;j<=N;j+=i)primes[i]=0;
+}
